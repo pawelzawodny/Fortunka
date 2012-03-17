6542da3f442a66069381811496ec449d5d85969f
Pawel Zawodny <pawel.zawodny@gmail.com>
2012-03-17 02:44:24 +0100

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ Fortunes::Application.routes.draw do resources :fortunes
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'fortunes#index'
 
   # See how all your routes lay out with "rake routes"
 
